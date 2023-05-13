73f11b448ed9379d04f6fbab2e3b5d907256a390
disappointedButNotSuprised <submarine@disroot.org>
2023-05-14 00:25:36 +0200

    first working MLP

---
+++ MLP/MLP.py
@@ -0,0 +1,82 @@
+# MLP algorithm
+from random import seed
+from random import randrange
+from csv import reader
+import random
+import matplotlib.pyplot as plt
+import copy
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.neural_network import MLPClassifier
+from sklearn.datasets import load_iris
+import numpy as np
+
+# Load a CSV file
+def load_csv(filename):
+	dataset = list()
+	with open(filename, 'r') as file:
+		csv_reader = reader(file)
+		for row in csv_reader:
+			if not row:
+				continue
+			dataset.append(row)
+	return dataset
+
+# Convert string column to float
+def str_column_to_float(dataset, column):
+	for row in dataset:
+		row[column] = float(row[column].strip())
+
+
+
+###########################
+# TESTING
+###########################
+
+# load and prepare data
+filename = 'iris.csv'
+dataset = load_csv(filename)
+for i in range(len(dataset[0])-1):
+	str_column_to_float(dataset, i)
+
+iris = load_iris()
+X = iris.data[:, (2, 3)]
+y = (iris.target == 0).astype(np.int_)
+
+mlp = MLPClassifier(max_iter=1, learning_rate_init=0.01, random_state=None, warm_start=True)
+
+# Pre-define the axes for plotting
+axes = [0, 7, 0, 3]
+
+# Pre-generate a grid of sampling points
+x0, x1 = np.meshgrid(
+        np.linspace(axes[0], axes[1], 200).reshape(-1, 1),
+        np.linspace(axes[2], axes[3], 200).reshape(-1, 1),
+    )
+
+# Now, show the change after fitting epoch by epoch
+for epochs in range(0,10):
+    
+    # Fit the model
+    mlp.fit(X, y)
+    
+    # Plot the dataset
+    plt.figure(figsize=(10, 4))
+    plt.plot(X[y==1, 0], X[y==1, 1], "yo", label="Iris-Setosa")
+    plt.plot(X[y==0, 0], X[y==0, 1], "bs", label="Not Iris-Setosa")
+    
+    # Use to model to sampling predictions over all feature space
+    y_predict = mlp.predict(np.c_[x0.ravel(), x1.ravel()])
+    zz = y_predict.reshape(x0.shape)
+    
+    # get a nice color
+    from matplotlib.colors import ListedColormap
+    custom_cmap = ListedColormap(['#9898ff', '#fafab0'])
+    
+    # Use contour plot again
+    plt.contourf(x0, x1, zz, cmap=custom_cmap)
+    plt.xlabel("Petal length", fontsize=14)
+    plt.ylabel("Petal width", fontsize=14)
+    plt.legend(loc="upper left", fontsize=14)
+    plt.axis(axes)
+    plt.show()
