fb680b13c67b21eae2a006d72117157a6acd88ac
disappointedButNotSuprised <submarine@disroot.org>
2023-05-20 19:55:05 +0200

    complete overhaul

--- MLP/MLP.py
+++ MLP/MLP.py
@@ -1,77 +1,41 @@
 # MLP algorithm
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.neural_network import MLPClassifier
-from sklearn.datasets import load_iris
-from matplotlib.colors import ListedColormap
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-import numpy as np
 from sklearn import datasets
+from sklearn.datasets import load_iris
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
 
+# load the dataset into parameter and target value vectors, then divide into train and test subsets
+iris = datasets.load_iris()
+X, y = datasets.load_iris(return_X_y = True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-###########################
-# TESTING
-###########################
+# create classifier
+mlp = MLPClassifier(max_iter=100, learning_rate_init=0.01, warm_start=True)
 
-#iris = load_iris()
-#X = iris.data[:, (2, 3)] 
-#X = iris.data[:, (0, 1, 2, 3)]
-#y = (iris.target == 0).astype(np.int_)
-X, y = datasets.load_iris(return_X_y=True)
-mlp = MLPClassifier(max_iter=1, learning_rate_init=0.01, warm_start=True)
-# warm_start - retain the solution of the previous call to fit as initialization
+# Fit the model and make a prediction
+y_predict = mlp.fit(X_train, y_train).predict(X_test)
 
-# Pre-define the axes for plotting
-axes = [0, 7, 0, 3]
+# generate metrics
+conf_matrix = confusion_matrix(y_test, y_predict)
+clf_report = classification_report(y_test, y_predict, target_names = iris.target_names, output_dict=True)
 
-# Pre-generate a grid of sampling points
-x0, x1, x2, x3 = np.meshgrid(
-        np.linspace(axes[0], axes[1], 50).reshape(-1, 1),
-        np.linspace(axes[0], axes[1], 50).reshape(-1, 1),
-        np.linspace(axes[0], axes[1], 50).reshape(-1, 1),
-        np.linspace(axes[2], axes[3], 50).reshape(-1, 1),   
-    )
+# print confusion matrix
+plt.figure()
+ax = sns.heatmap(conf_matrix, annot=True, fmt='d')
+ax.set_title("Confusion matrix for MLP algorithm", pad=20, fontweight ="bold")
+ax.set_xlabel("Predicted Diagnosis", fontsize=12, labelpad=10)
+ax.xaxis.set_ticklabels(['Setosa', 'Virginica', 'Versicolor'])
+ax.set_ylabel("Actual Diagnosis", fontsize=12, labelpad=10)
+ax.yaxis.set_ticklabels(['Setosa', 'Virginica', 'Versicolor'])
 
-results = list()
-
-# Now, show the change after fitting epoch by epoch
-for epochs in range(0,100):
-    
-    # Fit the model
-    mlp.fit(X, y)
-
-    # Use to model to sampling predictions over all feature space
-    y_predict = mlp.predict(np.c_[x0.ravel(), x1.ravel(), x2.ravel(), x3.ravel()])
-    result = y_predict.reshape(x0.shape)
-    
-    results.append(result)
-
-# prepare plot and colors
-fig, axs = plt.subplots(nrows = 3, ncols = 2, figsize = (10,8)) 
-fig.tight_layout()
-fig.subplots_adjust(hspace=0.5)
-viridis_big = mpl.colormaps['viridis']
-custom_cmap = ListedColormap(viridis_big(np.linspace(0.6, 0.75, 128)))
-
-iterator = 1
-for n in range(2):
-    for i in range(3):
-        # plot the dataset
-        axs[i, n].plot(X[y==1, 0], X[y==1, 1], "o", label="Iris-Setosa", c = "#598cbd")
-        axs[i, n].plot(X[y==0, 0], X[y==0, 1], "o", label="Not Iris-Setosa", c = "#3e6182")
-
-        # plot contour plot
-        color = axs[i, n].contourf(x0, x1, results[iterator][1], cmap=custom_cmap)
-        axs[i, n].set_title('Run for epoch nr: ' + str(iterator), fontweight ="bold", fontsize=10, loc='left')
-        axs[i, n].set_xlabel("Petal length", fontsize=10)
-        axs[i, n].set_ylabel("Petal width", fontsize=10)
-        axs[i, n].axis(axes)
-        iterator = iterator + (n+1) * (i+1)
-
-# legend and colorbar
-cbar = fig.colorbar(color, orientation = 'horizontal', ax=axs.ravel().tolist(), ticks=[0, 1], aspect = 50, shrink = 0.95)
-cbar.ax.set_xticklabels(['not-setosa', 'setosa'])
-axs[2,1].legend(loc='lower center', bbox_to_anchor = (-0.1, -0.7) , fancybox=True, shadow = True, ncols = 2)
+# print classification report
+plt.figure()
+ax = sns.heatmap(pd.DataFrame(clf_report).iloc[:-1, :].T, annot=True, )
+ax.set_title("Classification report for MLP algorithm", pad=20, fontweight ="bold")
 
 plt.show()
